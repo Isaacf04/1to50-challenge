acb2f7f96b3e2a5b552f8390e9149c5d14db217e
Luis Zambra <luis.zambra@abstracta.com.uy>
2020-04-24 15:49:12 -0300

    refactored code

--- webdriverio-example/src/specs/game.spec.js
+++ webdriverio-example/src/specs/game.spec.js
@@ -1,31 +1,17 @@
-describe('Beating the game', function()  {
+describe('Beating the game', function () {
     it('Should navigate to the site', () => {
-        browser.url(`${browser.options.baseUrl}`);
-        $('#grid').waitForDisplayed();
+      browser.url(`${browser.options.baseUrl}`);
+      $('#grid').waitForDisplayed();
     });
     it('Should tap on the grid until I beat the game', () => {
-        // Guardo los cuadrados en un array
-        let positions = Array(51).fill("");
-        for(i = 1; i <= 25; i++) {
-            element = $('#grid').$(`div:nth-child(${i})`);
-            positions[parseInt(element.getText())] = element;
-        }
-        /*
-        Clickeo los primeros 25 cuadrados, y a medida que se actualizan voy guardando los nuevos números generados.
-        Cuando paso de los primeros 25 simplemente me limito a clickearlos.
-        */
-        for(i = 1; i < 26; i++) {
-            positions[i].click();
-            // Espero a que cambie el número
-            browser.pause(150);
-            // Recibo otro número > 25 y lo guardo en el array
-            positions[parseInt(positions[i].getText())] = positions[i];
-        }
-        for(i = 26; i < 51; i++) {
-            positions[i].click();
-        }
+      // Clickeo en los números en orden
+      const grid = $('#grid');
+      for (i = 1; i < 51; i++) {
+        grid.$(`div=${i}`).click();
+      }
     });
     it('Should get the score', () => {
-        browser.saveScreenshot('./src/screenshots/screenshot.png');
-    })
-})
+      browser.saveScreenshot('./src/screenshots/screenshot.png');
+    });
+  });
+  
--- webdriverjs-example/src/specs/game.spec.js
+++ webdriverjs-example/src/specs/game.spec.js
@@ -1,63 +1,49 @@
 const fs = require('fs');
 const webdriver = require('selenium-webdriver'),
-    By = webdriver.By,
-    until = webdriver.until;
+  By = webdriver.By,
+  until = webdriver.until;
 
-    webdriver.promise.USE_PROMISE_MANAGER = false;
+webdriver.promise.USE_PROMISE_MANAGER = false;
 
-describe('Beating the game', function() {
-    let driver;
+const chrome = require('selenium-webdriver/chrome');
+const path = require('chromedriver').path;
 
-    before(function() {
-        driver = new webdriver.Builder().forBrowser('chrome').build();
+const service = new chrome.ServiceBuilder(path).build();
+chrome.setDefaultService(service);
+
+describe('Beating the game', function () {
+  let driver;
+
+  before(function () {
+    driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
+  });
+
+  after(function () {
+    driver.quit();
+  });
+
+  it('Should navigate to the site', async function () {
+    this.timeout(20000);
+    driver.get('http://zzzscore.com/1to50/en/');
+    //const grid = driver.findElement(By.id('grid'));
+    const grid = await driver.findElement(By.id('grid'));
+    await driver.wait(until.elementIsVisible(grid), 1000);
+  });
+
+  it('Should tap on the grid until I beat the game', async function () {
+    this.timeout(20000);
+    for (i = 1; i < 51; i++) {
+      let element = await driver.findElement(By.xpath(`//*[@id='grid']/div[text()="${i}"]`));
+      await element.click();
+    }
+  });
+
+  it('Should get the score', () => {
+    driver.takeScreenshot().then(function (data) {
+      var base64Data = data.replace(/^data:image\/png;base64,/, '');
+      fs.writeFile('./src/screenshots/screenshot.png', base64Data, 'base64', function (err) {
+        if (err) console.log(err);
+      });
     });
-
-    after(function() {
-        driver.quit();
-    });
-
-    it('Should navigate to the site', async function() {
-        this.timeout(10000);
-        driver.get('http://zzzscore.com/1to50/en/');
-        //const grid = driver.findElement(By.id('grid'));
-        const grid = await driver.findElement(By.id('grid'));
-        await driver.wait(until.elementIsVisible(grid), 1000);
-    });
-
-    it('Should tap on the grid until I beat the game', async function() {
-        this.timeout(20000);
-        // Guardo los cuadrados en un array
-        let positions = Array(51).fill("");
-        for(i = 1; i <= 25; i++) {
-            let element = await driver.findElement(By.css(`#grid > div:nth-child(${i})`));
-            let index = await element.getText();
-            positions[parseInt(index)] = element;
-        }
-        /*
-        Clickeo los primeros 25 cuadrados, y a medida que se actualizan voy guardando los nuevos números generados.
-        Cuando paso de los primeros 25 simplemente me limito a clickearlos.
-        */
-        for(i = 1; i < 26; i++) {
-            let element = await positions[i];
-            await element.click();
-            // Espero a que cambie el número
-            await driver.sleep(150);
-            // Recibo otro número > 25 y lo guardo en el array
-            let index = await element.getText();
-            positions[parseInt(index)] = element;
-        }
-        for(i = 26; i < 51; i++) {
-            let element = await positions[i];
-            await element.click();
-        }
-    });
-
-    it('Should get the score', () => {
-        driver.takeScreenshot().then(function(data){
-            var base64Data = data.replace(/^data:image\/png;base64,/,"")
-            fs.writeFile("./src/screenshots/screenshot.png", base64Data, 'base64', function(err) {
-                 if(err) console.log(err);
-            });
-         });
-    });
-})
+  });
+});
